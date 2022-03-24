d55fac0a3b379ac1f34b4fffc6b26d4a695f5650
Mrgracey <gracey.martin@cts.edu.ar>
2022-03-23 23:59:19 -0300

    first commit

---
+++ src/index.js
@@ -0,0 +1,15 @@
+//React packages
+import ReactDOM from "react-dom";
+
+//Components
+import App from './App';
+
+//Context
+
+//Styles
+import './index.css'
+
+ReactDOM.render(
+    <App />, 
+    document.querySelector("#root")
+);
